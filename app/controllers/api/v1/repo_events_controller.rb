449e91c254bead67e2f3cdccc840f45ded7b9574
Marina Murashev <marina@hoteltonight.com>
2017-12-27 04:38:40 -0500

    error response for bad params to api

--- app/controllers/api/v1/repo_events_controller.rb
+++ app/controllers/api/v1/repo_events_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::RepoEventsController < ApplicationController
     user = params[:user]
     repo_name = params[:repo_name]
 
-    @events = Clients::Github.fetch_events(user: user, repo_name: repo_name)
+    if user && repo_name
+      @events = Clients::Github.fetch_events(user: user, repo_name: repo_name)
+    else
+      render json: { error: "required params: user, repo_name" }, status: :bad_request
+    end
   end
 end
--- spec/requests/api/v1/repo_events_spec.rb
+++ spec/requests/api/v1/repo_events_spec.rb
@@ -1,19 +1,45 @@
 require "rails_helper"
 
 describe Api::V1::RepoEventsController, "GET#index" do
-  it "responds with status of 200 when all is well" do
-    allow(Clients::Github).to receive(:fetch_events).and_return({})
+  let(:user) { "uzer" }
+  let(:repo_name) { "ripo_naym" }
+  let(:valid_params) { { user: user, repo_name: repo_name } }
 
-    get api_v1_repo_events_path
-
-    expect(response.status).to eq 200
-  end
-
-  it "calls the Github client with parameters" do
+  it "responds with status of 200 and calls github client with params when all is well" do
     allow(Clients::Github).to receive(:fetch_events)
 
-    get api_v1_repo_events_path, params: { user: "uzer", repo_name: "ripo_naym" }
+    get api_v1_repo_events_path, params: valid_params
 
-    expect(Clients::Github).to have_received(:fetch_events).with(user: "uzer", repo_name: "ripo_naym")
+    expect(response.status).to eq 200
+    expect(Clients::Github).to have_received(:fetch_events).with(user: user, repo_name: repo_name)
+  end
+
+  context "missing params" do
+    it "returns bad_request when user not provided" do
+      allow(Clients::Github).to receive(:fetch_events)
+
+      get api_v1_repo_events_path, params: valid_params.reject{ |k,v| k == :user }
+
+      expect(response.status).to eq 400
+      expect(Clients::Github).to_not have_received(:fetch_events)
+    end
+
+    it "returns bad_request when repo_name not provided" do
+      allow(Clients::Github).to receive(:fetch_events)
+
+      get api_v1_repo_events_path, params: valid_params.reject{ |k,v| k == :repo_name }
+
+      expect(response.status).to eq 400
+      expect(Clients::Github).to_not have_received(:fetch_events)
+    end
+
+    it "returns bad_request when user and repo_name not provided" do
+      allow(Clients::Github).to receive(:fetch_events)
+
+      get api_v1_repo_events_path, params: valid_params.reject{ |k,v| [:repo_name, :user].include? k }
+
+      expect(response.status).to eq 400
+      expect(Clients::Github).to_not have_received(:fetch_events)
+    end
   end
 end
